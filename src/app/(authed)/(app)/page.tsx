e13dfca2557d81fd67c3439b3574b5d7e1a3eb70
Achuth <armorachyuth123@gmail.com>
2024-02-20 14:09:05 +0000

    tinybird files

--- src/app/(authed)/(app)/page.tsx
+++ src/app/(authed)/(app)/page.tsx
@@ -1,7 +1,12 @@
 import getUserId from "@/lib/auth";
 
 export default async function Home() {
-  const tenantId = getUserId()
+  const tenantId = getUserId();
+  const events = await getEvents({
+    tenantId,
+    since: Date.now() - 1000 * 60 * 60 * 24 * 7,
+  });
+
   return (
     <main className="">
       Feedz
